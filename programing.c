699091fd77a3f8a1b3555ff6fcd01f6b3ce48970
Billy19092024 <billyjoshuaishimwe@gmail.com>
2025-10-09 22:45:34 +0200

    system to calculate average and sum of student with C programing

--- programing.c
+++ programing.c
@@ -1,10 +1,26 @@
 #include <stdio.h>
 int main (){
+    int math,physics, english, programming, statistics;
+    int sum ;
+    float avg;
+      // Input 5 subjects
+    printf("enter scores for 5 subject:\n");
+    printf("math :"); scanf("%d", &math);
+   printf("Physics: "); scanf("%d", &physics);
+    printf("English: "); scanf("%d", &english);
+    printf("Programming: "); scanf("%d", &programming);
+    printf("Statistics: "); scanf("%d", &statistics);
 
-    int age= 25; // init is keywords, age is an identifier
-    float marks=95.9; //  float is keywords, marks is idendifier 
-    printf("%d\n", age); 
-    printf("%.2f\n", marks);
-    return 0 ; // return is keyword
+ // Calculate sum and average
 
+     sum = math + physics + english + programming + statistics;
+    avg = sum / 5.0;
+     // Display values
+      printf("\nScores:\n");
+    printf("Math = %d\nPhysics = %d\nEnglish = %d\nProgramming = %d\nStatistics = %d\n",
+           math, physics, english, programming, statistics);
+    printf("Sum = %d\n", sum);
+    printf("Average = %.2f\n", avg);
+
+    return 0; 
 }

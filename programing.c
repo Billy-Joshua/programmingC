31fbab9e41aa5238a78839146f3d7d35b92c0831
Billy19092024 <billyjoshuaishimwe@gmail.com>
2025-10-09 21:54:03 +0200

    constant ,operation with C

--- programing.c
+++ programing.c
@@ -1,13 +1,10 @@
 #include <stdio.h>
-int main(){
-    int studentID = 25;
-    int studentage = 32;
-    float studentfees =74.5;
-    char studentgrade ='B';
-    printf("student id: %d\n, studentID");
-    printf("student age:%d\n",studentage);
-    printf("student fee:%f\n", studentfees);
-    printf("student grade:%c\n", studentgrade);
-    
-    return 0;
+int main() {
+  int passwordLength = 5;
+
+  printf("%d\n", passwordLength >= 8); // 0 (false), too short
+  printf("%d\n", passwordLength < 8);  // 1 (true), needs more characters
+
+  return 0;
 }
+  

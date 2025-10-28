7ab208e4b6417065b60942fc31da80e69d24c5d1
Billy19092024 <billyjoshuaishimwe@gmail.com>
2025-10-28 21:24:13 +0200

    addition

--- programing.c
+++ programing.c
@@ -1,25 +1,9 @@
 #include <stdio.h>
-int main (){
-    int math,physics, english, programming, statistics;
-    int sum ;
-    float avg;
-      // Input 5 subjects
-    printf("enter scores for 5 subject:\n");
-    printf("math :"); scanf("%d", &math);
-   printf("Physics: "); scanf("%d", &physics);
-    printf("English: "); scanf("%d", &english);
-    printf("Programming: "); scanf("%d", &programming);
-    printf("Statistics: "); scanf("%d", &statistics);
-
- // Calculate sum and average
-
-     sum = math + physics + english + programming + statistics;
-    avg = sum / 5.0;
-     // Display values
-      printf("\nScores:\n");
-    printf("Math = %d\nPhysics = %d\nEnglish = %d\nProgramming = %d\nStatistics = %d\n",
-           math, physics, english, programming, statistics);
-    printf("Sum = %d\n", sum);
-    printf("Average = %.2f\n", avg);
-
+int add(int a,int b){
+return a+b;
+}
+int main(){
+    int sum = add(4 ,5);
+    printf("sum=%d\n",sum);
+    return 0;
 }
